84c32d6c81e6dcde1d49b3eb0c512d92332881fd
Mohammad Jahangir Hossain <jenisaniaini@gmail.com>
2026-06-11 17:09:04 +0300

    Add scope to manifest and force SW update

--- sw.js
+++ sw.js
@@ -3,3 +3,4 @@ const urlsToCache = ['/', '/index.html', '/manifest.json', 'https://cdnjs.cloudf
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); self.skipWaiting(); });
 self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(response => response || fetch(event.request).then(fetchResponse => { return caches.open(CACHE_NAME).then(cache => { cache.put(event.request, fetchResponse.clone()); return fetchResponse; }); })).catch(() => caches.match('/index.html'))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
+// Force update Thu Jun 11 17:09:04 +03 2026
